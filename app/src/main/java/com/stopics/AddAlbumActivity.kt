4a408ced86c6c0448eb365270a3bb84379f1259d
Samuel <95430052+SamuelWyckaert@users.noreply.github.com>
2022-10-28 10:02:31 +0200

    tp4

--- app/src/main/java/com/stopics/AddAlbumActivity.kt
+++ app/src/main/java/com/stopics/AddAlbumActivity.kt
@@ -1,13 +1,39 @@
 package com.stopics
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.stopics.model.Album
+import com.stopics.model.Picture
+import com.stopics.storage.AlbumJSONFileStorage
 
 class AddAlbumActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_album)
-
+        val btnAdd = findViewById<Button>(R.id.create_album)
+        btnAdd.setOnClickListener (View.OnClickListener {
+            addAlbum()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun addAlbum(){
+        val title_text = findViewById<EditText>(R.id.title_add_album)
+        var storageAlbum = AlbumJSONFileStorage(this)
+        var newAlbum = Album(1, title_text.text.toString(), true, "", mutableListOf<Picture>())
+        storageAlbum.insert(newAlbum)
+        val list_storage = storageAlbum.findAll()
+
+        Log.d("READ", list_storage[0].toString())
+
     }
 }
--- app/src/main/java/com/stopics/TestActivity.kt
+++ app/src/main/java/com/stopics/TestActivity.kt
@@ -1,24 +1,45 @@
 package com.stopics
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.ImageRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.stopics.model.Album
 import com.stopics.storage.AlbumJSONFileStorage
 import com.stopics.storage.utility.file.JSONFileStorage
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.jar.Manifest
 
 class TestActivity: AppCompatActivity() {
+    companion object {
+        private val IMAGE_CHOOSE = 1000;
+        private val PERMISSION_CODE = 1001;
+    }
     val url = "http://51.68.95.247/gr-3-2/album.json"
-
+    val url_image_1 = "http://51.68.95.247/gr-3-2/ski-1.png"
+    lateinit var imageDownloadView: ImageView
+    lateinit var imageView: ImageView
+    private var imageUri : Uri? = null
 
 
 
@@ -29,14 +50,68 @@ class TestActivity: AppCompatActivity() {
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
+        imageView = findViewById(R.id.imageView)
+        imageDownloadView = findViewById(R.id.imageDownload)
         val btn = findViewById<Button>(R.id.btn)
+        val btn_picture = findViewById<Button>(R.id.btn_gallery)
         btn.setOnClickListener(View.OnClickListener {
             Log.e("ICI", "ICI")
             val queue = Volley.newRequestQueue(this)
-            queue.add(request)
+            //queue.add(request)
             queue.start()
         })
+
+
+        btn_picture.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE)
+
+                }
+                else{
+                    choosePicture();
+                }
+            }
+            else{
+                choosePicture();
+            }
+        }
     }
+
+    private fun choosePicture(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_CHOOSE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    choosePicture()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && data != null){
+           imageUri = data.data
+            Log.d("image url", imageUri.toString())
+           imageView.setImageURI(imageUri)
+        }
+    }
+
 
 
     val request = JsonObjectRequest(
@@ -48,18 +123,40 @@ class TestActivity: AppCompatActivity() {
             Toast.makeText(this, R.string.success, Toast.LENGTH_SHORT).show()
             Log.d("JSON", res.toString())
             val json_value = AlbumJSONFileStorage(this)
-            json_value.write()
             val obj = json_value.jsonToObject(res)
+            json_value.insert(obj)
             Log.d("OBJECT", obj.toString())
-            val read_obj = AlbumJSONFileStorage(this).find(1)
-            Log.d("READDDDD", read_obj.toString())
+            val list_storage = json_value.findAll()
 
-
+            Log.d("READ", list_storage[0].toString())
 
         },
         { err ->
             Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show()
             Log.e("ICI", "ERREUR")}
     )
+
+
+
+    /*val requestDownload = ImageRequest()
+        ImageRequest(
+        url_image_1,
+        Response.Listener<Bitmap>() {
+            fun onResponse(bitmap: Bitmap) {
+                imageDownloadView.setImageBitmap(bitmap)
+                Log.d("IMG", "GAGNE")
+            }
+        }, 0, 0, null,
+        Response.ErrorListener() {
+            override fun onErrorResponse(error: VolleyError){
+                Log.d("IMG", "erreur")
+
+            }
+
+        })*/
+
+
+
+
 
 }
